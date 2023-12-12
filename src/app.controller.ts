816d859ecc53ac7cf7e70ecba2640e763ce0c3fd
gychoi7 <haruhunter7@gmail.com>
2023-12-12 11:29:22 +0900

    add first

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import {Controller, Get, Post} from '@nestjs/common';
+import {Controller, Get, Param, Post} from '@nestjs/common';
 import { AppService } from './app.service';
 import axios from "axios";
 import {ConfigService} from "@nestjs/config";
@@ -20,12 +20,13 @@ export class AppController {
     return this.appService.getHello();
   }
 
-  @Get("/account")
-  async getAccount(): Promise<any> {
-    console.log(process.env.OPENSEA_API_KEY)
+  @Get(":id")
+  //Param에서 address를 받아서 그 값을 options.url에 넣어준다.
+
+  async getAccount(@Param('id') id:string): Promise<any> {
     const options = {
       method: 'GET',
-      url: 'https://api.opensea.io/api/v2/accounts/0xeE0E7938949573bfC2B21A5E4b7dc10494607a4c',
+      url: `https://api.opensea.io/api/v2/accounts/${id}`,
       headers: {accept: 'application/json', 'x-api-key': this.configService.get<string>('OPENSEA_API_KEY')}
     };
     const res = await axios.request(options);
